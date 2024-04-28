d0fe4ad80444472a441eb1f307a835707d58385a
leji-a <toviya3135@gmail.com>
2024-04-27 21:04:39 -0300

    Rust and TOML support added

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
+				ensure_installed = { "lua_ls", "tsserver", "pylsp", "rust_analyzer" },
 			})
 		end,
 	},
@@ -19,6 +19,7 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
+
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
@@ -28,7 +29,14 @@ return {
 			lspconfig.html.setup({
 				capabilities = capabilities,
 			})
-			--add more
+			lspconfig.pylsp.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+
+			-- Add more
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -4,3 +4,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>7", function()
+  vim.cmd(":nohlsearch")
+end, { noremap = true })
