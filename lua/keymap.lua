9a44fe3e7e9c1890e570a46431a426a50673dd10
leji-a <toviya3135@gmail.com>
2024-05-20 16:45:29 -0300

    Transparency added and fixed notify error

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,21 +1,26 @@
 -- Neovim
 vim.keymap.set("n", "<leader>7", function()
-  vim.cmd(":nohlsearch")
+	vim.cmd(":nohlsearch")
 end, { noremap = true })
 -- None-ls
 vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 -- Neo tree
-vim.keymap.set("n", "<C-f>", ":Neotree toggle filesystem reveal left<CR>")
+vim.keymap.set("n", "<C-f>", "<cmd>Neotree toggle filesystem reveal left<cr>")
 -- Oil vim
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
 -- Twilight
-vim.keymap.set("n", "<C-t>", ":Twilight<CR>", { desc = "Open twilight" })
+vim.keymap.set("n", "<C-t>", "<cmd>Twilight<cr>", { desc = "Open twilight" })
 -- Compiler
-vim.api.nvim_set_keymap('n', '<F6>', "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F6>", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
 -- Redo last selected option
-vim.api.nvim_set_keymap('n', '<S-F6>',
-     "<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
-  .. "<cmd>CompilerRedo<cr>",
- { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<S-F6>",
+	"<cmd>CompilerStop<cr>" -- (Optional, to dispose all tasks before redo)
+		.. "<cmd>CompilerRedo<cr>",
+	{ noremap = true, silent = true }
+)
 -- Toggle compiler results
-vim.api.nvim_set_keymap('n', '<S-F7>', "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<S-F7>", "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
+-- Telescope oldfiles
+vim.keymap.set("n", "<leader>r", "<cmd>Telescope oldfiles<cr>")
